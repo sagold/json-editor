dbc0c77f67d227e94bfb0fdd31eaceec384fa6fc
Sascha Goldhofer <github@saschagoldhofer.de>
2022-09-29 18:25:01 +0200

    refactor: extract text editor to separate module

--- packages/react-json-editor/src/lib/editors/StringEditor.tsx
+++ packages/react-json-editor/src/lib/editors/StringEditor.tsx
@@ -29,42 +29,6 @@ export const StringEditorPlugin: EditorPlugin = {
     Editor: StringEditor
 };
 
-export const TextEditor = editor<StringNode, string>(({ node, setValue }) => {
-    const isValidConst = node.schema.const != null && node.errors.length === 0;
-    const disabled = node.options.disabled || isValidConst;
-    return (
-        <div data-type="string" data-id={node.pointer} className={disabled ? 'disabled' : 'enabled'}>
-            <Form.Field
-                error={node.errors.length > 0 && node.errors.map((e) => e.message)}
-                disabled={node.options.disabled}
-            >
-                <label>{node.options.title as string}</label>
-                <TextareaAutosize
-                    id={node.id}
-                    disabled={disabled}
-                    rows={1}
-                    minRows={2}
-                    maxRows={10}
-                    cacheMeasurements
-                    value={node.value}
-                    onHeightChange={(height, instance) => console.log(height, instance)}
-                    onChange={(e) => {
-                        setValue(e.target.value || '');
-                    }}
-                />
-            </Form.Field>
-            {<div className="description">{node.options.description as string}</div>}
-        </div>
-    );
-});
-
-export const TextEditorPlugin: EditorPlugin = {
-    id: 'text-editor',
-    use: (node) =>
-        node.schema.type === 'string' && (node.schema.format === 'html' || node.schema.format === 'textarea'),
-    Editor: TextEditor
-};
-
 export const SelectEditor = editor<StringNode, string>(({ node, setValue }) => {
     const enumValues = (node.schema.enum || []) as string[];
 
