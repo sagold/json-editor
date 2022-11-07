1e2a84618ff236239b6dde316292238d57fa0626
Sascha Goldhofer <github@saschagoldhofer.de>
2022-11-07 13:42:06 +0100

    fix: typo from bulk refactoring

--- packages/react-json-editor/src/lib/stories/UndoRedo.stories.tsx
+++ packages/react-json-editor/src/lib/stories/UndoRedo.stories.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { useJsonEditor, defaultWidgets } from '../../index';
-import { HistoryPlugin, HistoryPlugineditor } from 'headless-json-editor';
+import { HistoryPlugin, HistoryPluginInstance } from 'headless-json-editor';
 import { Form, Button, Icon } from 'semantic-ui-react';
 import { ComponentStory } from '@storybook/react';
 import '../styles.scss';
@@ -43,7 +43,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<any> = ({ data, schema }) => {
     const [node, editor] = useJsonEditor({ data, schema, widgets: defaultWidgets, plugins: [HistoryPlugin] });
-    const historyPlugin = useRef<HistoryPlugineditor>(editor.plugin('history') as HistoryPlugineditor);
+    const historyPlugin = useRef<HistoryPluginInstance>(editor.plugin('history') as HistoryPluginInstance);
     const Widget = editor.getWidget(node);
     const history = historyPlugin.current;
     const isUndoEnabled = history ? history.getUndoCount() > 0 : false;
