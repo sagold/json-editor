15f6ee85360e5f773b0ac6c49bfce3b3412f9076
Sascha Goldhofer <github@saschagoldhofer.de>
2022-11-29 19:36:35 +0100

    remove: nx

--- packages/react-json-editor/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import rjeTheme from './react-json-editor-theme';
-
-addons.setConfig({
-    theme: rjeTheme
-});
--- packages/rje-code-widgets/.storybook/preview.js
+++
@@ -1,2 +0,0 @@
-import 'semantic-ui-css/semantic.min.css';
-// import '@sagold/react-json-editor/index.css';
