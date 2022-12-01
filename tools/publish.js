a701807f737f6a406b6ab1974402b6a3a91b75da
Sascha Goldhofer <github@saschagoldhofer.de>
2022-12-01 20:34:27 +0100

    release: bump version to v0.19.2

--- tools/publish.js
+++ tools/publish.js
@@ -3,10 +3,14 @@ const path = require('path');
 const sh = require('./sh');
 
 async function publishPackages() {
-    const cmd = `npm publish`;
-    console.log(await sh(cmd, path.resolve('./packages/headless-json-editor')));
-    console.log(await sh(cmd, path.resolve('./packages/react-json-editor')));
-    console.log(await sh(cmd, path.resolve('./packages/rje-code-widgets')));
+    try {
+        const cmd = `npm publish`;
+        console.log(await sh(cmd, path.resolve('./packages/headless-json-editor')));
+        console.log(await sh(cmd, path.resolve('./packages/react-json-editor')));
+        console.log(await sh(cmd, path.resolve('./packages/rje-code-widgets')));
+    } catch (e) {
+        console.log('error', e);
+    }
 }
 
 publishPackages();
