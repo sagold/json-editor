f170da0d91e8d89f9793b8599a138ab83a4986f5
Sascha Goldhofer <github@saschagoldhofer.de>
2022-12-04 23:15:36 +0100

    test: setup jest testing

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            // required due to custom location of tsconfig.json configuration file
+            // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
+            { tsconfig: './tsconfig.spec.json' }
+        ]
+    },
+    moduleFileExtensions: ['ts', 'js', 'html']
+};
