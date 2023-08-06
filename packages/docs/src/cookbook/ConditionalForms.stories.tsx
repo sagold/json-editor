247e7d6f7dd6a5efd703036a4ccd1c0eadbb68c3
Sascha Goldhofer <post@saschagoldhofer.de>
2023-08-07 00:34:31 +0200

    docs: update cookbock and code styles

---
+++ packages/docs/src/cookbook/ConditionalForms.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { JsonForm, JsonSchema } from '@sagold/react-json-editor';
+import { widgets, Theme } from '@sagold/rje-widgets';
+
+const meta: Meta<typeof ConditionalFormStory> = {
+    component: ConditionalFormStory
+};
+
+export default meta;
+type Story = StoryObj<typeof ConditionalFormStory>;
+
+function ConditionalFormStory() {
+    return (
+        <Theme>
+            <JsonForm
+                widgets={widgets}
+                addOptionalProps={false}
+                schema={
+                    {
+                        type: 'object',
+                        title: 'allOf with single if-then-else statements',
+                        required: ['trigger'],
+                        properties: {
+                            trigger: {
+                                title: 'with additional field',
+                                type: 'boolean',
+                                default: false
+                            },
+                            optional: {
+                                options: { hidden: true, title: false },
+                                type: 'object',
+                                required: ['title'],
+                                properties: {
+                                    title: {
+                                        title: 'Optional form',
+                                        type: 'string'
+                                    }
+                                }
+                            }
+                        },
+                        allOf: [
+                            {
+                                if: {
+                                    required: ['trigger'],
+                                    properties: {
+                                        trigger: {
+                                            const: true
+                                        }
+                                    }
+                                },
+                                then: {
+                                    required: ['optional'],
+                                    properties: {
+                                        optional: {
+                                            options: { hidden: false },
+                                            minLength: 1
+                                        }
+                                    }
+                                }
+                            }
+                        ]
+                    } as JsonSchema
+                }
+            />
+        </Theme>
+    );
+}
+
+export const ConditionalForm: Story = {};
