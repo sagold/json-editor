b7815cda286cb9f0dc3e9a8b589886e75dd35085
Sascha Goldhofer <post@saschagoldhofer.de>
2024-03-08 16:37:10 +0100

    update: usePlugin to accept undefined editor instances

--- packages/react-json-editor/src/lib/usePlugin.ts
+++ packages/react-json-editor/src/lib/usePlugin.ts
@@ -7,15 +7,17 @@ import { JsonEditor } from "./JsonEditor";
  * register a plugin-instance to json-editor
  * Note: plugins having a state will be reset
  */
-export function usePlugin<T extends Plugin>(editor: JsonEditor, plugin: T, options?: Record<string, unknown>): ReturnType<T> | undefined {
+export function usePlugin<T extends Plugin>(editor?: JsonEditor, plugin?: T, options?: Record<string, unknown>): ReturnType<T> | undefined {
     const ref = useRef<Record<string, unknown>>();
     const optionsChanged = options !== ref.current && !deepEqual(options, ref.current);
     return useMemo(() => {
-        ref.current = options;
-        const instance = editor.addPlugin(plugin, options) as ReturnType<T>;
-        if (instance) {
-            editor.plugins = editor.plugins.filter(p => p === instance || p.id !== instance.id);
-            return instance;
+        if (editor && plugin) {
+            ref.current = options;
+            const instance = editor.addPlugin(plugin, options) as ReturnType<T>;
+            if (instance) {
+                editor.plugins = editor.plugins.filter(p => p === instance || p.id !== instance.id);
+                return instance;
+            }
         }
         return undefined;
     },
