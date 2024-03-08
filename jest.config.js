9a3772dbce36b41e24e89b491cdb0d0580308796
Sascha Goldhofer <post@saschagoldhofer.de>
2024-03-08 14:41:13 +0100

    build: simplify test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    projects: [
-        "packages/*/jest.config.ts"
-    ]
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            // required due to custom location of tsconfig.json configuration file
+            // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
+            { tsconfig: './tsconfig.spec.json' }
+        ]
+    },
+    modulePathIgnorePatterns: ['dist'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'html']
 };
