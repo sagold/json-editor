139ca02216fb1eb9ae414010087a84e98621a4dc
Sascha Goldhofer <github@saschagoldhofer.de>
2024-12-10 17:21:52 +0100

    docs: update roadmap

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
     base: 'light',
     brandTitle: '@sagold/json-editor',
-    // brandUrl: 'https://github.com/sagold/json-editor',
+    brandUrl: 'https://github.com/sagold/json-editor',
     brandTarget: '_self'
 });
