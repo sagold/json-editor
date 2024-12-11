e87f0a93f5248659c4b7e3d5d9217b414e6c86ab
Sascha Goldhofer <github@saschagoldhofer.de>
2024-12-11 17:21:35 +0100

    docs: add first changelog

--- tools/publish.js
+++ tools/publish.js
@@ -6,7 +6,7 @@ const getPackages = require('./getPackages');
 async function publishPackages() {
     const packages = getPackages();
     try {
-        const cmd = `npm publish`;
+        const cmd = `npm publish --access=public`;
         for (let i = 0; i < packages.length; i += 1) {
             console.log(await sh(cmd, path.resolve(`./packages/${packages[i]}`)));
         }
